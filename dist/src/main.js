1826f6b02e45aa80d197bd5dab7ba00508e925db
bvsvntv <developer.basanta@gmail.com>
2026-01-27 17:15:38 +0545

    Pass file name as argument and execute it

--- dist/src/main.js
+++ dist/src/main.js
@@ -2,10 +2,26 @@ import { readFileSync } from 'node:fs';
 import { Lexer } from './lexer.js';
 import { Parser } from './parser.js';
 import { Interpreter } from './interpreter.js';
-const source = readFileSync(process.cwd() + '/examples/init.ga', 'utf-8');
-const lexer = new Lexer(source);
-const tokens = lexer.readTokens();
-const parser = new Parser(tokens);
-const stmts = parser.parse();
-const interpreter = new Interpreter();
-interpreter.interpret(stmts);
+function getFileNameFromArgs(args) {
+    if (args.length > 1) {
+        console.log('> Too many arguments.');
+    }
+    const fileName = args[0] || null;
+    return fileName;
+}
+function execFile(path) {
+    const source = readFileSync(path, 'utf-8');
+    const lexer = new Lexer(source);
+    const tokens = lexer.readTokens();
+    const parser = new Parser(tokens);
+    const stmts = parser.parse();
+    const interpreter = new Interpreter();
+    interpreter.interpret(stmts);
+}
+function main() {
+    const fileName = getFileNameFromArgs(process.argv.slice(2));
+    if (fileName !== null) {
+        execFile(fileName);
+    }
+}
+main();
--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,32 @@ import { Lexer } from './lexer.js';
 import { Parser } from './parser.js';
 import { Interpreter } from './interpreter.js';
 
-const source = readFileSync(process.cwd() + '/examples/init.ga', 'utf-8');
-const lexer = new Lexer(source);
-const tokens = lexer.readTokens();
+function getFileNameFromArgs(args: string[]): string | null {
+  if (args.length > 1) {
+    console.log('> Too many arguments.');
+  }
 
-const parser = new Parser(tokens);
-const stmts = parser.parse();
+  const fileName = args[0] || null;
+  return fileName;
+}
 
-const interpreter = new Interpreter();
-interpreter.interpret(stmts);
+function execFile(path: string): void {
+  const source = readFileSync(path, 'utf-8');
+  const lexer = new Lexer(source);
+  const tokens = lexer.readTokens();
+
+  const parser = new Parser(tokens);
+  const stmts = parser.parse();
+
+  const interpreter = new Interpreter();
+  interpreter.interpret(stmts);
+}
+
+function main(): void {
+  const fileName = getFileNameFromArgs(process.argv.slice(2));
+  if (fileName !== null) {
+    execFile(fileName);
+  }
+}
+
+main();
